7d3f53ea7f03944738688c32362b65ade5570e92
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-24 22:27:50 -0600

    Starting repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+	res.send('<h1>Testing here<h1>');
+});
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.log(`App running on port ${port}`));
